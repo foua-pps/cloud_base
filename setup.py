54fc7bc61aec4f5f767d975dbb7b35fe9d16a2c7
kaur.inderpreet <inderpreet.kaur@smhi.se>
2024-04-16 12:08:12 +0200

    setup file for pip packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import re
+import setuptools
+
+here = Path(__file__).parent.absolute()
+required = [
+    r for r in (here / 'requirements.txt').read_text().splitlines()
+    if '=' in r or "git" in r
+]
+version = re.findall(
+    r'__version__ *= *[\'"]([^\'"]+)',
+    (here / 'cbase' / '__init__.py').read_text(encoding='utf-8')
+)[-1]
+
+long_description = """
+    Package for a cloud base product using VIIRS
+"""
+
+setuptools.setup(
+    name='cbase',
+    version=version,
+    description='Package for cloud base algorithm',
+    author='Inderpreet Kaur',
+    author_email='inderpreet.kaur@smhi.se',
+    url='http://nwcsaf.org',
+    long_description=long_description,
+    license='GPL',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9, <4',
+    install_requires=required,
+    include_package_data=True,
+)
