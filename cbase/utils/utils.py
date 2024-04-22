c9234d538dd955b202e403efefa4038229ec0fed
kaur.inderpreet <inderpreet.kaur@smhi.se>
2024-04-22 22:20:56 +0200

    added haversine function

--- cbase/utils/utils.py
+++ cbase/utils/utils.py
@@ -3,6 +3,9 @@ import numpy as np
 from datetime import datetime, timedelta
 
 
+R = 6371.0  # Earth's radius in kilometers
+
+
 @dataclass
 class BaseDate:
     """
@@ -23,3 +26,27 @@ def convert2datetime(times: np.array, base_date_string: BaseDate) -> np.array:
 
     base_date = datetime.strptime(base_date_string.base_date, "%Y%m%d%H%M")
     return np.array([base_date + timedelta(seconds=value) for value in times])
+
+
+def haversine_distance(
+    lat1: float, lon1: float, lat2: np.array, lon2: np.array
+) -> np.array:
+    """
+    find distance between two points on earth
+    """
+    lat1_rad = np.radians(lat1)
+    lon1_rad = np.radians(lon1)
+    lat2_rad = np.radians(lat2)
+    lon2_rad = np.radians(lon2)
+
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    # Calculate the Haversine distance
+    a = (
+        np.sin(dlat / 2) ** 2
+        + np.cos(lat1_rad) * np.cos(lat2_rad) * np.sin(dlon / 2) ** 2
+    )
+    c = 2 * np.arcsin(np.sqrt(a))
+    distance = R * c
+    return distance
