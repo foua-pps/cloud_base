b672db3ff78145b9841a613971bf0aeab07e9936
Inderpreet Kaur <sm_indka@freja1.nsc.liu.se>
2024-05-14 21:31:10 +0200

    adapted paths

--- cbase/scripts/run_matching.py
+++ cbase/scripts/run_matching.py
@@ -2,16 +2,15 @@ from cbase.data_readers import cloudsat, viirs, era5
 from cbase.matching.match_vgac_cloudsat_nwp import DataMatcher
 
 
-filename = "/home/a002602/data/cloud_base/cloudsat/2018150015649_64371_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
-vgac_file = (
-    "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0130_n002738_K005.nc"
-)
-era5_file = "/home/a002602/data/cloud_base/NWP/GAC_ECMWF_ERA5_201801010100+000H00M"
+filename = (
+        "/nobackup/smhid19/proj/foua/data/satellite/cloudsat/2B_GEORPOF_V5/2018/2018150015649_64371_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
+        )
 
-filename1 = "/home/a002602/data/cloud_base/cloudsat/2018150033525_64372_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
-vgac_file1 = (
-    "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0312_n002739_K005.nc"
+vgac_file = (
+    "/nobackup/smhid17/proj/foua/data/satellit/VGAC/2018/05/2018VGAC_VJ102MOD_A2018150_0130_n002738_K005.nc"
 )
+era5_file = "/nobackup/smhid20/proj/safnwccm/data/nwp/ERA5/2018/05/GAC_ECMWF_ERA5_201805010000+000H00M"
+
 
 
 cld = cloudsat.CloudsatData.from_file(filename)
