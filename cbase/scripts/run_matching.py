5dca746c7d1a3e10bd10e1a3ee3e31934cdc9ec2
kaur.inderpreet <inderpreet.kaur@smhi.se>
2024-05-14 11:32:56 +0200

    updated script to include multiprocessing and CLI

--- cbase/scripts/run_matching.py
+++ cbase/scripts/run_matching.py
@@ -1,28 +1,88 @@
+from pathlib import Path
+import multiprocessing
+from sys import argv
+import argparse
+from cbase.matching.match_csat_vgac_nwp_filenames import (
+    get_matching_cloudsat_vgac_nwp_files,
+)
 from cbase.data_readers import cloudsat, viirs, era5
 from cbase.matching.match_vgac_cloudsat_nwp import DataMatcher
 
 
-filename = "/home/a002602/data/cloud_base/cloudsat/2018150015649_64371_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
-vgac_file = (
-    "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0130_n002738_K005.nc"
-)
-era5_file = "/home/a002602/data/cloud_base/NWP/GAC_ECMWF_ERA5_201801010100+000H00M"
+# filename = "/home/a002602/data/cloud_base/cloudsat/2018150015649_64371_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
+# vgac_file = (
+#     "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0130_n002738_K005.nc"
+# )
+# era5_file = "/home/a002602/data/cloud_base/NWP/GAC_ECMWF_ERA5_201801010100+000H00M"
 
-filename1 = "/home/a002602/data/cloud_base/cloudsat/2018150033525_64372_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
-vgac_file1 = (
-    "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0312_n002739_K005.nc"
-)
+# filename1 = "/home/a002602/data/cloud_base/cloudsat/2018150033525_64372_CS_2B-GEOPROF_GRANULE_P1_R05_E07_F03.hdf"
+# vgac_file1 = (
+#     "/home/a002602/data/cloud_base/vgac/VGAC_VJ102MOD_A2018150_0312_n002739_K005.nc"
+# )
 
 
-cld = cloudsat.CloudsatData.from_file(filename)
+def process(cloudsat_file: Path, vgac_file: Path, nwp_file: Path):
+    """main process"""
+    print("starting main  process", cloudsat_file)
+    vgc = viirs.VGACData.from_file(vgac_file)
+    nwp = era5.Era5.from_file(nwp_file.absolute().as_posix())
+    cld = cloudsat.CloudsatData.from_file(cloudsat_file.absolute().as_posix())
 
-vgc = viirs.VGACData.from_file(vgac_file)
-nwp = era5.Era5.from_file(era5_file)
+    # create matching object
+    dm = DataMatcher(cld, vgc, nwp)
+    dm.match_vgac_cloudsat()
+    dm.create_cnn_dataset_with_nwp()
 
-dm = DataMatcher(cld, vgc, nwp)
 
-dm.match_vgac_cloudsat()
+def cli(args_list: list[str]) -> None:
+    """command line args"""
+    parser = argparse.ArgumentParser(
+        description="Find matching Cloudsat, VGAC and NWP (ERA5) files"
+    )
+    parser.add_argument(
+        "-C",
+        "--cloudsat_files",
+        type=str,
+        nargs="+",
+        help="Full path to Cloudsat level1b file(s)",
+        metavar="CLOUDSAT_FILE",
+        required=True,
+    )
+    parser.add_argument(
+        "-V",
+        "--vgac_files",
+        type=str,
+        nargs="+",
+        help="Full path to available VGAC file(s).",
+        metavar="VGAC_FILE",
+        required=True,
+    )
+    parser.add_argument(
+        "-N",
+        "--nwp_files",
+        type=str,
+        nargs="+",
+        help="Full path to available NWP (ERA5) file(s).",
+        metavar="NWP_FILE",
+        required=True,
+    )
+    args = parser.parse_args(args_list)
+    return get_matching_cloudsat_vgac_nwp_files(
+        cfiles=[Path(f) for f in args.cloudsat_files],
+        vfiles=[Path(f) for f in args.vgac_files],
+        nfiles=[Path(f) for f in args.nwp_files],
+    )
 
-dm.create_cnn_dataset_with_nwp()
 
-# check data
+# read in command line args and get list of files to process
+cloudsat_files, vgac_files, nwp_files = cli(argv[1:])
+
+# Create a multiprocessing Pool
+with multiprocessing.Pool() as pool:
+    # Map the function to the list of files
+    pool.starmap(
+        process,
+        zip(cloudsat_files, vgac_files, nwp_files),
+    )
+
+# process(filename, vgac_file, era5_file)
