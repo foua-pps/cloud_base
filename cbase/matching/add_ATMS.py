bc4edc4c80f73c6de1e05c3ee10c0e2e079b5d42
sm_indka <inderpreet.kaur@smhi.se>
2024-10-04 15:38:02 +0200

    made it comptabile with run script

--- cbase/matching/add_ATMS.py
+++ cbase/matching/add_ATMS.py
@@ -1,7 +1,8 @@
 import os
-
+from typing import Dict
 from dataclasses import dataclass
 import xarray as xr
+from xarray import Dataset, DataArray
 import numpy as np
 from pathlib import Path
 import glob
@@ -10,7 +11,7 @@ from datetime import datetime, timedelta
 from atrain_match.utils.match import match_lonlat
 from scipy.interpolate import LinearNDInterpolator
 from cbase.data_readers.atms import ATMSData
-from cbase.matching.config import ATMS_PARAMETERS, ATMS_PATH
+from cbase.matching.config import ATMS_PARAMETERS
 from cbase.utils.utils import (
     extract_timestamp_from_atms_filename,
     check_lon_range,
@@ -35,20 +36,19 @@ class LatLonBox:
 class MatchATMSVGAC:
     """class to match ATMS data to VGAC scenes created for CNN"""
 
-    def __init__(self, vgacfile):
+    def __init__(self, vgacfile: Path, atmspath: Path, outpath: Path):
         self.vgac_file = vgacfile
-        self.outfile = os.path.join(
-            os.path.dirname(self.vgac_file), "ATMS", os.path.basename(vgacfile)
-        )
+        self.outfile = os.path.join(outpath, os.path.basename(vgacfile))
 
         self.vgac = xr.open_dataset(vgacfile.as_posix())
-        self.atms_files = self.find_matching_atms()
+        self.atms_files = self.find_matching_atms(atmspath)
         if len(self.atms_files) == 0:
             self.atms = None
+            print(f"No match found for {vgacfile}")
         else:
             self.atms = ATMSData.from_file(self.atms_files)
 
-    def find_matching_atms(self) -> list:
+    def find_matching_atms(self, atmspath: Path) -> list:
         """find matching ATMS files within +- TDIFF of VGAC file"""
         vgc_time = datetime.fromtimestamp(self.vgac.time.values[0, 0], pytz.utc)
         time_string = (
@@ -56,19 +56,22 @@ class MatchATMSVGAC:
             f"{vgc_time.strftime('%m')}"
             f"{vgc_time.strftime('%d')}T"
         )
-        atmsfiles = sorted(glob.glob(f"{ATMS_PATH}*{time_string}*"))
+
+        atmsfiles = sorted(glob.glob(f"{atmspath}/*{time_string}*"))
         date_str = np.array(
             [extract_timestamp_from_atms_filename(atmsfile) for atmsfile in atmsfiles]
         )
+
         tdiff_mask = (date_str > vgc_time - timedelta(minutes=VGAC_ATMS_TDIFF)) & (
             date_str < vgc_time + timedelta(minutes=VGAC_ATMS_TDIFF)
         )
+
         if tdiff_mask is None:
             return np.array([])
 
         return np.array(atmsfiles)[tdiff_mask]
 
-    def matching(self) -> dict:
+    def matching(self):
         """match the ATMS data to VGAC grid and write out a netcdf file"""
         if self.atms is None:
             interpolated_atms = self.add_fillvalue_atms_data()
@@ -77,7 +80,7 @@ class MatchATMSVGAC:
             self.vgac["longitude"] = adapt_lonrange(self.vgac.longitude, lon_string)
             self.atms.longitude = adapt_lonrange(self.atms.longitude, lon_string)
 
-            latlon_box = self.get_latlon_bounds(buffer=1.0)
+            latlon_box = self.get_latlon_bounds()
             latlon_mask = self.get_latlon_mask(latlon_box)
             matcher_mask = self.get_closest_matches(latlon_mask)
 
@@ -86,7 +89,7 @@ class MatchATMSVGAC:
         self.vgac = add2vgac_dataset(self.vgac, interpolated_atms)
         self.vgac.to_netcdf(self.outfile)
 
-    def get_latlon_bounds(self, buffer=0):
+    def get_latlon_bounds(self, buffer=0) -> LatLonBox:
         """gives lat lon bounds of where VGAC scene is located"""
         return LatLonBox(
             self.vgac.latitude.values.min() - buffer,
@@ -95,7 +98,7 @@ class MatchATMSVGAC:
             (self.vgac.longitude.values).max() + buffer,
         )
 
-    def get_latlon_mask(self, latlon_box: LatLonBox) -> np.ndarray:
+    def get_latlon_mask(self, latlon_box: LatLonBox) -> np.ndarray[np.bool_]:
         """gives a mask for ATMS data according to VGAC bounds"""
         return (
             (self.atms.latitude >= latlon_box.lower_lat)
@@ -104,7 +107,7 @@ class MatchATMSVGAC:
             & (self.atms.longitude <= latlon_box.right_lon)
         )
 
-    def get_closest_matches(self, latlon_mask: np.ndarray) -> np.ndarray:
+    def get_closest_matches(self, latlon_mask: np.ndarray) -> np.ndarray[np.bool_]:
         """find closest matches of ATMS with VGAc using atrain _match"""
         source = (self.vgac.longitude.values, self.vgac.latitude.values)
         target = (self.atms.longitude[latlon_mask], self.atms.latitude[latlon_mask])
@@ -114,7 +117,7 @@ class MatchATMSVGAC:
         )
         return np.any(distances > 0, axis=1)
 
-    def interpolate_atms2vgac(self, latlon_mask, matcher_mask):
+    def interpolate_atms2vgac(self, latlon_mask, matcher_mask) -> Dict[str, np.ndarray]:
         """interpolates the ATMS TBs to VGAC grid"""
         interpolated_atms = {}
         for parameter in ATMS_PARAMETERS:
@@ -131,20 +134,20 @@ class MatchATMSVGAC:
             )
         return interpolated_atms
 
-    def add_fillvalues_atms_data(self):
-        """if no ATMS file sare present, fillvalues are written to final file"""
+    def add_fillvalue_atms_data(self) -> Dict[str, np.ndarray]:
+        """if no ATMS files are present, fillvalues are written to final file"""
         fill_value_atms = {}
         for parameter in ATMS_PARAMETERS:
-            fill_value_atms[parameter] = -999.9 * np.ones_like(self.vgac.latitude.shape)
+            fill_value_atms[parameter] = -999.9 * np.ones_like(self.vgac.latitude)
         return fill_value_atms
 
 
-def make_dataarray(data, dims, coords):
+def make_dataarray(data, dims, coords) -> DataArray:
     """make a xarray dataarray"""
     return xr.DataArray(data=data, dims=dims, coords=coords)
 
 
-def add2vgac_dataset(vgac, interpolated_atms):
+def add2vgac_dataset(vgac, interpolated_atms) -> Dataset:
     """add ATMS paramters to existing VGAC dataset"""
     dims = ["npix", "nscan"]
     coords = dict(
