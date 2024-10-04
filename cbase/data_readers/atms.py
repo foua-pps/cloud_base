8e92a5584b11ecf918d877fbe55c427bc9b87c0d
sm_indka <inderpreet.kaur@smhi.se>
2024-10-04 09:28:33 +0200

    proper handinling of Nan in time data

--- cbase/data_readers/atms.py
+++ cbase/data_readers/atms.py
@@ -10,16 +10,17 @@ from datetime import datetime
 from cbase.utils.utils import datetime64_to_datetime, microseconds_to_datetime
 
 atms_data = {
-    "latitude" : [],
-    "longitude" : [],
+    "latitude": [],
+    "longitude": [],
     "time": [],
-    "tb17" : [],
+    "tb17": [],
     "tb18": [],
     "tb19": [],
     "tb20": [],
     "tb21": [],
-    "tb22": []
+    "tb22": [],
 }
+
 
 @dataclass
 class ATMSData:
@@ -39,15 +40,14 @@ class ATMSData:
     tb20: np.ndarray
     tb21: np.ndarray
     tb22: np.ndarray
-        
+
     @classmethod
     def from_file(cls, atmsfiles: list):
         """read data from netCDF file"""
         for atmsfile in sorted(atmsfiles):
-            print(atmsfile)
             with xr.open_dataset(atmsfile) as da:
                 atms_data["latitude"].append(da.lat.values)
-                atms_data["longitude"].append(da.lon.values)
+                atms_data["longitude"].append(da.lon.values % 360)
                 atms_data["tb17"].append(da.antenna_temp.values[:, :, 16])
                 atms_data["tb18"].append(da.antenna_temp.values[:, :, 17])
                 atms_data["tb19"].append(da.antenna_temp.values[:, :, 18])
@@ -60,42 +60,55 @@ class ATMSData:
         return ATMSData(
             np.concatenate(atms_data["latitude"]),
             np.concatenate(atms_data["longitude"]),
+            np.concatenate(atms_data["time"]),
             np.concatenate(atms_data["tb17"]),
             np.concatenate(atms_data["tb18"]),
             np.concatenate(atms_data["tb19"]),
             np.concatenate(atms_data["tb20"]),
             np.concatenate(atms_data["tb21"]),
             np.concatenate(atms_data["tb22"]),
-            np.concatenate(atms_data["time"])              
-        )        
-                
+        )
+
 
 def convert_to_datetime(utc_array) -> np.ndarray:
     """convert ATMS timestamps to datetime
     ATMS time stamps come as tuples of 8 values
     pertaining to names of the elements of UTC when
-    it is expressed as an array of 
+    it is expressed as an array of
     integers year,month,day,hour,minute,second,
     millisecond,microsecond
     """
-    year = utc_array[:, :, 0].astype(int)
-    month = utc_array[:, :, 1].astype(int)
-    day = utc_array[:, :, 2].astype(int)
-    hour = utc_array[:, :, 3].astype(int)
-    minute = utc_array[:, :, 4].astype(int)
-    second = utc_array[:, :, 5].astype(int)
-    nrows = utc_array.shape[0]
-    ncols = utc_array.shape[1]
 
-    # Create datetime objects
-    datetime_objects = np.array(
-        [[datetime(year[i, j], month[i, j], day[i, j],
-                    hour[i, j], minute[i, j], second[i, j])
-          for j in range(ncols)] for i in range(nrows)]
+    year = utc_array[:, :, 0].astype(float)
+    month = utc_array[:, :, 1].astype(float)
+    day = utc_array[:, :, 2].astype(float)
+    hour = utc_array[:, :, 3].astype(float)
+    minute = utc_array[:, :, 4].astype(float)
+    second = utc_array[:, :, 5].astype(float)
+
+    datetime_objects = np.full(year.shape, None, dtype=object)
+    nan_mask = (
+        np.isnan(year)
+        | np.isnan(month)
+        | np.isnan(day)
+        | np.isnan(hour)
+        | np.isnan(minute)
+        | np.isnan(second)
     )
-    
+    datetime_objects[nan_mask] = np.nan
+    valid_mask = ~nan_mask
+    # Create datetime objects
+    datetime_objects[valid_mask] = np.array(
+        [
+            datetime(int(y), int(m), int(d), int(h), int(mi), int(s))
+            for y, m, d, h, mi, s in zip(
+                year[valid_mask],
+                month[valid_mask],
+                day[valid_mask],
+                hour[valid_mask],
+                minute[valid_mask],
+                second[valid_mask],
+            )
+        ]
+    )
     return datetime_objects
-
-
-
-
