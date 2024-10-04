edef7054c391a1e083787d5addcd35234252db99
sm_indka <inderpreet.kaur@smhi.se>
2024-10-04 15:38:28 +0200

    script to add ATMS with cli

--- cbase/scripts/run_adding_ATMS.py
+++ cbase/scripts/run_adding_ATMS.py
@@ -9,16 +9,29 @@ def main():
     parser = argparse.ArgumentParser(
         description="Match ATMS data to VGAC scenes for CNN processing."
     )
-    # argument for vgacfile
     parser.add_argument(
-        "vgacpath", type=str, help="Path to the VGAC files used for matching"
+        "--vgacpath",
+        type=str,
+        required=True,
+        help="Path to the VGAC files to be populated with ATMS data",
+    )
+    parser.add_argument(
+        "--atmspath",
+        type=str,
+        required=True,
+        help="Path to the data directory containing ATMS input files",
+    )
+    parser.add_argument(
+        "--outpath",
+        type=str,
+        required=True,
+        help="Path to the output data directory containing ATMS populated files",
     )
     args = parser.parse_args()
     vgacfiles = glob.glob(os.path.join(args.vgacpath, "cnn*2012*"))
-
     for vgacfile in vgacfiles:
         print(vgacfile)
-        matcher = MatchATMSVGAC(Path(vgacfile))
+        matcher = MatchATMSVGAC(Path(vgacfile), Path(args.atmspath), Path(args.outpath))
         matcher.matching()
 
 
