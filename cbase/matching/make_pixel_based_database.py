cce8fcfac2b96d81545907d0a6adf3752712fa02
sm_indka <inderpreet.kaur@smhi.se>
2024-11-25 15:36:10 +0100

    ATMS now a functionality

--- cbase/matching/make_pixel_based_database.py
+++ cbase/matching/make_pixel_based_database.py
@@ -9,8 +9,9 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+N = 5
 
-KEYS = [
+VGAC_KEYS = [
     "time",
     "cloud_base",
     "cloud_top",
@@ -36,6 +37,13 @@ KEYS = [
     "rh900",
     "rh950",
     "rh1000",
+    "rh250",
+    "rh500",
+    "rh700",
+    "rh850",
+    "rh900",
+    "rh950",
+    "rh1000",
     "h_2meter",
     "ciwv",
     "tclw",
@@ -44,17 +52,32 @@ KEYS = [
     "vis_optical_depth",
     "latitude",
     "longitude",
+    "satzenith",
+    "satazimuth",
+]
+
+VGAC_PPS_KEYS = [
     "cth",  # PPS paramters
     "ctp",
     "ctt",
     "ctp_quality",
     "ct",
+    "ctp16",
+    "cth16",
+    "ctt16",
+    "ctp84",
+    "cth84",
+    "ctt84",
     "ct_quality",
     "cmic_phase",
     "cmic_quality",
     "cmic_lwp",
+    "cmic_cot",
     "elevation",
     "land_use",
+]
+
+ATMS_KEYS = [
     "tb17",  # ATMS parameters
     "tb18",
     "tb19",
@@ -64,7 +87,8 @@ KEYS = [
     "view_ang",
 ]
 
-KEYS_NEIGHBOURHOOD = [
+
+VGAC_KEYS_NEIGHBOURHOOD = [
     "tb11coldest",
     "tb12coldest",
     "tb11warmest",
@@ -74,6 +98,9 @@ KEYS_NEIGHBOURHOOD = [
     "var_M12",
     "var_M15",
     "var_M16",
+]
+
+ATMS_KEYS_NEIGHBOURHOOD = [
     "var_tb17",
     "var_tb18",
     "var_tb19",
@@ -82,12 +109,15 @@ KEYS_NEIGHBOURHOOD = [
     "var_tb22",
 ]
 
-ELIGIBLE_CHANNELS = [
+ELIGIBLE_VGAC_CHANNELS = [
     "M05",
     "M07",
     "M12",
     "M15",
     "M16",
+]
+
+ELIGIBLE_ATMS_CHANNELS = [
     "tb17",
     "tb18",
     "tb19",
@@ -97,7 +127,9 @@ ELIGIBLE_CHANNELS = [
 ]
 
 
-def cth2asl(da: Dict[str, Dict[str, np.ndarray]]) -> Dict[str, Dict[str, np.ndarray]]:
+def cth2asl(
+    da: Dict[str, Dict[str, np.ndarray]]
+) -> Dict[str, Dict[str, np.ndarray]]:
     """convert pps cth to above sea level"""
     da["cth"]["data"] = da["cth"]["data"] - da["elevation"]["data"]
     return da
@@ -109,53 +141,63 @@ def find_coldest_warmest_temp_in_neigh(
     """warmest and coldest pixels in neighborhood"""
     coldest = []
     warmest = []
-
+    imax = cb.shape[0]
+    jmax = cb.shape[1]
     indices = np.argwhere(cb[:, :] > 0)
     for ix, iy in indices:
-        ix1 = max([0, ix - 5])
-        ix2 = min([100, ix + 5])
-        iy1 = max([0, iy - 5])
-        iy2 = min([20, iy + 5])
+        ix1 = max([0, ix - N])
+        ix2 = min([imax, ix + N])
+        iy1 = max([0, iy - N])
+        iy2 = min([jmax, iy + N])
         coldest.append(tb[ix1:ix2, iy1:iy2].flatten().min())
         warmest.append(tb[ix1:ix2, iy1:iy2].flatten().max())
     return np.array(coldest), np.array(warmest)
 
 
-def find_variance_in_neigh(ds: Dataset) -> Dict[str, float]:
-    variance = {}
+def find_variance_in_neigh(ds: Dataset, do_atms) -> Dict[str, float]:
+    all_channels = ELIGIBLE_VGAC_CHANNELS
+    if do_atms:
+        all_channels += ELIGIBLE_ATMS_CHANNELS
+    variance = {key: [] for key in all_channels}
     indices = np.argwhere(ds.cloud_base.values[:, :] > 0)
+    imax = ds.cloud_base.shape[0]
+    jmax = ds.cloud_base.shape[1]
     for ix, iy in indices:
-        ix1 = max([0, ix - 5])
-        ix2 = min([100, ix + 5])
-        iy1 = max([0, iy - 5])
-        iy2 = min([20, iy + 5])
-        for ichannel in ELIGIBLE_CHANNELS:
+        ix1 = max([0, ix - N])
+        ix2 = min([imax, ix + N])
+        iy1 = max([0, iy - N])
+        iy2 = min([jmax, iy + N])
+        for ichannel in all_channels:
             tbs = ds[ichannel][ix1:ix2, iy1:iy2].values
             tbs[tbs < 0] = np.nan
-            variance[ichannel] = np.nanvar(tbs)
+            variance[ichannel].append(np.nanvar(tbs))
     return variance
 
 
-def make_pixel_dataset(input_files: List[str]) -> Dict[str, Dict[str, np.ndarray]]:
+def make_pixel_dataset(
+    input_files: List[str], do_atms: bool = False
+) -> Dict[str, Dict[str, np.ndarray]]:
     """make xr dataset with all input parameters in keys"""
-    training_data = {key: [] for key in KEYS}
-    for key in KEYS_NEIGHBOURHOOD:
+    parameter_names = VGAC_KEYS + VGAC_PPS_KEYS
+    parameter_neighbourhood = VGAC_KEYS_NEIGHBOURHOOD
+    if do_atms:
+        parameter_names += ATMS_KEYS
+        parameter_neighbourhood += ATMS_KEYS_NEIGHBOURHOOD
+
+    training_data = {key: [] for key in parameter_names}
+    for key in parameter_neighbourhood:
         training_data[key] = []
 
     for input_file in tqdm(input_files[:]):
         with xr.open_dataset(input_file) as ds:
-            # ds = ds.sel(
-            # {
-            #     "npix": ds["npix"].values[8:-8],
-            #     "nscan": ds["nscan"].values[8:-8],
-            # }
-            # ).load()
             mask = ds.cloud_base.values > 0
-            for key in KEYS:
+            for key in parameter_names:
                 try:
                     training_data[key].append(ds[key].values[mask])
                 except KeyError:
-                    logging.warning(f"Parameter '{key}' is missing in the dataset.")
+                    logging.warning(
+                        f"Parameter '{key}' is missing in the dataset."
+                    )
                     continue
                 except Exception as e:
                     logging.error(
@@ -175,16 +217,16 @@ def make_pixel_dataset(input_files: List[str]) -> Dict[str, Dict[str, np.ndarray
             training_data["tb12warmest"].append(warm_tb12)
 
             variance = find_variance_in_neigh(ds)
+
             for key in variance:
                 key_name = "var_" + key
-                print(variance[key])
                 training_data[key_name].append(variance[key])
 
     da = {}
-    print(training_data)
     for key in training_data:
         print(key)
         training_data[key] = np.concatenate(training_data[key])
+        print(training_data[key].shape)
         da[key] = {"dims": ("n"), "data": training_data[key]}
 
     # convert cth above sea level
